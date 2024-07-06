a5aac10891712c02c4cb008582e890ab088e1290
klleriston <klleandrade@outlook.com>
2024-07-06 18:15:49 -0300

    feat: implementing api for creating tasks with google calendar

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TaskModule, PrismaModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env'
+  }), AuthModule, TaskModule, PrismaModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { google } from 'googleapis';
+import * as fs from 'fs';
+
+@Injectable()
+export class AuthService {
+  private oauth2Client;
+
+  constructor() {
+    const keyFile = 'credentials.json';
+    const key = JSON.parse(fs.readFileSync(keyFile, 'utf8'));
+
+    this.oauth2Client = new google.auth.GoogleAuth({
+      credentials: key,
+      scopes: ['https://www.googleapis.com/auth/calendar'],
+    });
+  }
+
+  async createEvent(event) {
+    const auth = await this.oauth2Client.getClient();
+    const calendar = google.calendar({ version: 'v3', auth });
+    try {
+      const response = await calendar.events.insert({
+        calendarId: 'theriderofking@gmail.com',
+        requestBody: event,
+      });
+      console.log('Event created:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error creating event:', error);
+      throw error;
+    }
+  }
+}
